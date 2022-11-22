f01247e75a16166eb40b2bba9e06d9e2b357a871
Schleidens <alectineschleidens2016@gmail.com>
2022-11-22 16:00:54 -0500

    render the Header and design it

--- src/components/Moviepage.jsx
+++ src/components/Moviepage.jsx
@@ -5,6 +5,8 @@ import Header from './layout/Header';
 export default function Moviepage() {
   const [movies, setMovies] = useState([]);
   const url = "https://imdb-api.com/en/API/ComingSoon/k_p4nwmk5x"
+  const imgUrl = "https://cdn.pixabay.com/photo/2019/07/24/18/32/fantasy-4360934_960_720.jpg"
+
 
 
   const getPosts = () =>{
@@ -24,7 +26,8 @@ export default function Moviepage() {
 
     
     <div>
-      
+      <Header imgUrl={imgUrl} title={"lorem ipsum"} date={"23, 22, 2022"} actor={"Lorem Ipsum"}/>
+
       {movies.map((movie)=>(
         <li key={movie.id}>{movie.title}</li>
       ))}
