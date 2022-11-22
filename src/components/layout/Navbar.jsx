5adbe5f27da84a4fef514c1f918e8276ab224b3f
Schleidens <alectineschleidens2016@gmail.com>
2022-11-22 13:17:58 -0500

    create and design the navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <h1>
+            MoviZZZ
+        </h1>
+
+        <button>
+            Top 250 Movies
+        </button>
+    </div>
+  )
+}
