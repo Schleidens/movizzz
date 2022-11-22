c3a6536d3161aade25669b4db35cd8f07bf8d96a
Schleidens <alectineschleidens2016@gmail.com>
2022-11-22 13:42:34 -0500

    design the navbar and add it to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/layout/Navbar'
 import Moviepage from './components/Moviepage'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
      <Moviepage />
     </div>
   )
