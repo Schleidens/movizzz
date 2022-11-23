ca329efdeb730112efb7c2bd0d27d6c4238b8181
Schleidens <alectineschleidens2016@gmail.com>
2022-11-23 13:40:28 -0500

    design the header botto bar

---
+++ src/components/layout/Bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Bar({title}) {
+  return (
+    <div className='bar'>
+        <h3>
+            {title}
+        </h3>
+        <hr />
+    </div>
+  )
+}
