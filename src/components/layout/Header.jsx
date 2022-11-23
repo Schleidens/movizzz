3b76cf795f9bbc10fecab2014c02954591a82b2d
Schleidens <alectineschleidens2016@gmail.com>
2022-11-23 12:29:30 -0500

    redesign the header section

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,16 +5,23 @@ export default function Header({imgUrl, title, date, actor}) {
   return (
     <div className='header' style={{backgroundImage: `url(${imgUrl})`}}>
         <div>
+        <span>
+            {date}
+        </span>
         <h2>
             {title}
         </h2>
         <span>
-            {date}
-        </span>
-        <br />
-        <span>
             {actor}
         </span>
+        <div className='header__btn'>
+          <button>
+            play
+          </button>
+          <button>
+            Ad to wishlist
+          </button>
+        </div>
         </div>
     </div>
   )
