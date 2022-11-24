d9015d10b561730aea9cc70d290dfe1e1875fd3a
Schleidens <alectineschleidens2016@gmail.com>
2022-11-24 12:02:01 -0500

    adding loading component and set it when doing the fetch

--- src/components/Moviepage.jsx
+++ src/components/Moviepage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react';
 import Bar from './layout/Bar';
 import Header from './layout/Header';
+import Loading from './layout/Loading';
 import Movie from './layout/Movie';
 
 export default function Moviepage() {
@@ -9,10 +10,11 @@ export default function Moviepage() {
   const url = "https://imdb-api.com/en/API/ComingSoon/"
   const apikey ="k_p4nwmk5x"
   const imgUrl = "https://cdn.pixabay.com/photo/2019/07/24/18/32/fantasy-4360934_960_720.jpg"
-
+  const [isLoading, setIsLoading] = useState(false)
 
 
   const getPosts = async () =>{
+    setIsLoading(true);
 
    await fetch(url+apikey)
     .then(response => response.json())
@@ -20,6 +22,8 @@ export default function Moviepage() {
       setMovies(json.items)
     })
     .catch(error=> console.log(error))
+
+    setIsLoading(false);
   }
   
 
@@ -41,11 +45,15 @@ export default function Moviepage() {
      
       <Bar title={"COMING SOON"}/>
 
-      <div className="main__box">
-      {movies.map((movie)=>(
-        <Movie key={movie.id} img={movie.image} date={movie.releaseState} title={movie.title}/>
-      ))}
-      </div>
+      {isLoading ? 
+        <Loading />
+       : 
+        <div className="main__box">
+        {movies.map((movie)=>(
+          <Movie key={movie.id} img={movie.image} date={movie.releaseState} title={movie.title}/>
+        ))}
+        </div>
+      }
 
 
     </div>
