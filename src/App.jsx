ddfa83fb71bd0ed18fad23a8e2c4c1e53c8c8e0b
Schleidens <alectineschleidens2016@gmail.com>
2022-11-25 13:35:57 -0500

    created the Topmovies component and make it works, set routes for it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Navbar from './components/layout/Navbar'
 import Moviepage from './components/Moviepage'
+import Topmovies from './components/Topmovies'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 
       <Routes>
         <Route path='/' exact element={<Moviepage />} />
+        <Route path='/topmovies' element={<Topmovies />} />
       </Routes>
       
       <Footer />
---
+++ src/components/Topmovies.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import Bar from './layout/Bar';
+import Header from './layout/Header';
+import Loading from './layout/Loading';
+import Movie from './layout/Movie';
+
+export default function Moviepage() {
+  const [movies, setMovies] = useState([]);
+  const url = "https://imdb-api.com/en/API/Top250Movies/"
+  const apikey ="k_p4nwmk5x"
+  const imgUrl = "https://cdn.pixabay.com/photo/2019/07/24/18/32/fantasy-4360934_960_720.jpg"
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  const getPosts = async () =>{
+    setIsLoading(true);
+
+      await fetch(url+apikey)
+        .then(response => response.json())
+        .then(json => {
+          setMovies(json.items)
+        })
+        .catch(error=> console.log(error))
+
+    setIsLoading(false);
+  }
+  
+
+  useEffect(()=>{
+      getPosts();
+  }, []);
+
+
+  return (
+
+    
+    <div>
+      <Header imgUrl={imgUrl} title={"lorem ipsum"} date={"23, 22, 2022"} actor={"Lorem Ipsum"}/>
+
+     
+      <Bar title={"COMING SOON"}/>
+
+      {isLoading ? 
+        <Loading />
+       : 
+        <div className="main__box">
+        {movies.map((movie)=>(
+          <Movie key={movie.id} img={movie.image} date={movie.year} title={movie.title}/>
+        ))}
+        </div>
+      }
+
+
+    </div>
+  )
+}
