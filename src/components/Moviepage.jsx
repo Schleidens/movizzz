7c11eec43c86a00fd6f9261e01cd92007b7bb500
Schleidens <alectineschleidens2016@gmail.com>
2022-12-20 13:15:42 -0500

    clean code

--- src/components/Moviepage.jsx
+++ src/components/Moviepage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { useMemo } from 'react';
 import { useState } from 'react';
 import { usePagination } from './hooks/pagination';
 import Bar from './layout/Bar';
